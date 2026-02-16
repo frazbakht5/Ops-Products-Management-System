57e1e6ab13d947e2ffee90f6470975c81e68f5d7
Fraz Bakht <mr.fraz.bakht@gmail.com>
2026-02-16 05:29:35 +0500

    added cors

--- backend-server/src/app.ts
+++ backend-server/src/app.ts
@@ -1,9 +1,11 @@
 import express from 'express';
+import cors from 'cors';
 import 'reflect-metadata';
 import { registerRoutes } from './modules/route.index';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 registerRoutes(app);
